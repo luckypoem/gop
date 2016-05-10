5b1a4bd63973eed4c8774c8cd0e60dff57713b48
Phus Lu <phuslu@hotmail.com>
2016-05-10 20:26:53 +0800

    fetch latest version in gae.go

--- gae/gae.go
+++ gae/gae.go
@@ -281,9 +281,25 @@ func robots(rw http.ResponseWriter, r *http.Request) {
 func root(rw http.ResponseWriter, r *http.Request) {
 	context := appengine.NewContext(r)
 	version, _ := strconv.ParseInt(strings.Split(appengine.VersionID(context), ".")[1], 10, 64)
-	ctime := time.Unix(version/(1<<28)+8*3600, 0).Format(time.RFC3339)
-	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(rw, "GoProxy server %s works, deployed at %s\n", Version, ctime)
+	ctime := time.Unix(version/(1<<28), 0).Format(time.RFC3339)
+	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(rw, `
+<html>
+<head>
+	<title>GoProxy</title>
+</head>
+<body>
+<div>GoProxy server %s works, deployed at %s, latest version is <span id="github"></span></div>
+</body>
+<script type="text/javascript">
+	function callback(commits) {
+		date = commits['data'][0]['commit']['committer']['date']
+		document.getElementById('github').innerHTML = date
+	}
+</script>
+<script src="https://api.github.com/repos/phuslu/goproxy/commits?sha=server.gae&path=gae/gae.go&callback=callback"></script>
+</html>
+`, Version, ctime)
 }
 
 func init() {
